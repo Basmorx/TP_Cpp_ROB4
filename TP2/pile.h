47b7abf2394fd0f755e7a306c509a71829964be1
Mattéo Muzaton <>
2026-02-16 10:59:44 +0100

    Fin EXO 3 ET 4

--- TP2/pile.h
+++ TP2/pile.h
@@ -7,24 +7,80 @@
 template <typename T>
 class Pile{
 private : 
-unsigned int mMax;
-unsigned int mSommet;
-std::unique_ptr<T[]> mPile;
-
+    int mMax;
+    int mSommet;
+    T * mPile;
 public: 
+    //Constructeur et destructeur
     
-    Pile(int taille = 100);
-    Pile(const Pile& autre);
-    ~Pile();
+    Pile(int taille = 100){
+            if (taille > 0) {
+            mMax = taille;
+        } else {
+            
+            mMax = 100; 
+        }
+        mSommet = 0; 
+        
+        mPile = new T[mMax];
+        
+        std::cout << "Constructeur: Pile creee de taille " << mMax << std::endl;
+    }
+
+
+    Pile(const Pile& autre){
+        mMax = autre.mMax;
+        mSommet = autre.mSommet;
+        
+        mPile = new T[mMax];
+
+        for (int i = 0; i < mSommet; i++) {
+            mPile[i] = autre.mPile[i];
+        }
+
+        std::cout << "Constructeur de copie appele." << std::endl;
+    }
+
+    ~Pile(){
+        delete[] mPile;
+        std::cout << "Memoire liberee" << std::endl;
+    }
     
 
-    int compterElem();
-    void empilerElem(T elem);
-    T desempilerElem();
-    AfficherPile();
+    //Le reste des méthodes
 
+    int CompterElem(){
+        return mSommet;
+    }
 
+    void EmpilerElem(T elem){
+        if (mSommet < mMax) {
+            mPile[mSommet] = elem;
+            mSommet++;
+        } else {
+            std::cout << "La pile est pleine." << std::endl;
+        }
+    }
 
+    T DesempilerElem(){
+        if (mSommet > 0) {
+            T element = mPile[mSommet - 1];
+            mSommet--;
+            return element;
+        } else {
+            std::cout << "La pile est vide." << std::endl;
+            return '\0';
+        }
+    }
+
+    void AfficherPile(){
+        std::cout << "Contenu de la pile : ";
+        std::cout << "[ ";
+        for (int i = 0; i < mSommet; i++) {
+            std::cout << mPile[i] << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
 };
 
 
--- TP2/tp2ex1.cpp
+++ TP2/tp2ex1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include "piledechar.h"
+#include "pile.h"
 
 using namespace std;
 
@@ -12,6 +13,30 @@ void afficheInverse(PileDeChar p) {
     cout << endl;
 }
 
+PileDeChar inversemajuscule(PileDeChar p) {
+
+    PileDeChar pileTemp(p.CompterElements());
+    while (p.CompterElements() > 0) {
+        pileTemp.EmpilerElem(p.DesempilerElem());
+    }
+
+    PileDeChar nPile(pileTemp.CompterElements());
+
+    while (pileTemp.CompterElements() > 0) {
+        
+        char c = pileTemp.DesempilerElem(); 
+
+        // Logique de bascule Majuscule <-> Minuscule
+        if (std::isupper(c)) {
+            c = std::tolower(c); // Devient minuscule
+        } else if (std::islower(c)) {
+            c = std::toupper(c); // Devient majuscule
+        }
+        nPile.EmpilerElem(c);
+    }
+
+    return nPile;
+}
 
 int main() {
     char buffer[80];
@@ -26,6 +51,49 @@ int main() {
 
     cout << "Message a l'envers : ";
     afficheInverse(maPile);
+    PileDeChar MApILE = inversemajuscule(maPile);
+    MApILE.AffichePile();
 
+
+    // -------------------------------------------------
+    // TEST 1 : Pile<char> 
+    // -------------------------------------------------
+    cout << "=== TEST PILE<CHAR> ===" << endl;
+    Pile<char> pileChar(50);
+    
+    cout << "Entrez un mot : ";
+    // Lecture sécurisée demandée 
+    cin.getline(buffer, 79); 
+
+    // Empilage lettre par lettre
+    for (int i = 0; buffer[i] != '\0'; ++i) {
+        pileChar.EmpilerElem(buffer[i]);
+    }
+    
+    cout << "Contenu de la pile char : ";
+    pileChar.AfficherPile();
+
+    // -------------------------------------------------
+    // TEST 2 : Pile<int> 
+    // -------------------------------------------------
+    cout << "\n=== TEST PILE<INT> ===" << endl;
+    Pile<int> pileInt(10); // Pile d'entiers
+
+    // Empiler les valeurs 1, 2, 3 
+    cout << "Empilage des entiers..." << endl;
+    pileInt.EmpilerElem(1);
+    pileInt.EmpilerElem(2);
+    pileInt.EmpilerElem(3);
+
+    // Afficher la pile 
+    cout << "Etat de la pile int : ";
+    pileInt.AfficherPile();
+
+    // Dépiler et afficher le sommet obtenu 
+    int sommet = pileInt.DesempilerElem();
+    cout << "Valeur depilee (sommet) : " << sommet << endl;
+    
+    cout << "Etat final de la pile int : ";
+    pileInt.AfficherPile();
     return 0;
 }
