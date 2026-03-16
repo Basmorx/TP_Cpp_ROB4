6a1485cd6e30fb6843a7760fb668c67d0029fed0
Mattéo Muzaton <>
2026-03-16 13:56:15 +0100

    Co-authored-by: Basmorx <Basmorx@users.noreply.github.com>

--- Projet_CPP/compile.bash
+++ Projet_CPP/compile.bash
@@ -1,2 +1,5 @@
+source env_robotique/bin/activate
 rm ./run_tests
+export PKG_CONFIG_PATH=$PWD/env_robotique/lib/python3.12/site-packages/cmeel.prefix/lib/pkgconfig:$PKG_CONFIG_PATH
 g++ -std=c++17 test_joints.cpp -o run_tests $(pkg-config --cflags --libs eigen3 pinocchio) -lgtest -lgtest_main -pthread
+export LD_LIBRARY_PATH=$PWD/env_robotique/lib/python3.12/site-packages/cmeel.prefix/lib:$LD_LIBRARY_PATH
